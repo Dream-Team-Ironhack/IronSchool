8b43a4115ffbcf8c8aec5eef851686ba2f6959b9
Raphaela <raphaela.prammer@gmail.com>
2023-01-20 15:40:38 +0100

    basic class structure added

---
+++ src/main/java/org/example/Course.java
@@ -0,0 +1,65 @@
+package org.example;
+
+import java.util.List;
+
+public class Course {
+    private String courseId;
+    private String name;
+    private double price;
+    private double money_earned;
+    private Teacher teacher=null;
+    private List<Student> studentList;
+
+    public Course(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getMoney_earned() {
+        return money_earned;
+    }
+
+    public void setMoney_earned(double money_earned) {
+        this.money_earned = money_earned;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+}
